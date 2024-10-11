41f0afa5b3c3e331cca0e143c16c51279769bef5
ji3jieee <e1155980@u.nus.edu>
2024-10-12 02:41:09 +0800

    completed home page with the slider / roundttable for news and owrkshops

---
+++ home.js
@@ -0,0 +1,28 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        }
+    }
+  
+  });
